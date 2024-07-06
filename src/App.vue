574e210faf1e95de3fbe427166073bf9633e5137
GUILLAUME QUET <quet.guillaume.leo@gmail.com>
2024-07-06 15:31:25 +0100

    main setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row">
+    <g-slider label="slider" size="xs" />
+    <g-slider label="slider" size="xs" />
+    <g-slider label="slider" size="xs" />
+    <g-slider label="slider" size="xs" />
+    <g-slider label="slider" size="xs" />
+    <g-slider label="slider" size="xs" />
+    <g-slider label="slider" size="xs" />
+    <g-slider label="slider" size="xs" />
+    <g-slider label="slider" size="xs" />
+    <g-slider label="slider" size="xs" />
+    <g-slider label="slider" size="xs" />
+    <g-slider label="slider" size="xs" />
+  </div>
+</template>
+
+<script setup>
+import GSlider from "./components/library/sliders/GSlider.vue";
+</script>
